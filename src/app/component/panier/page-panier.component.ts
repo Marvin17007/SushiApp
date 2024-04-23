df4592bfd45a1147598f05edc47a4c06c810906b
Marvin17007 <61842931+Marvin17007@users.noreply.github.com>
2024-04-23 22:58:29 +0200

    Update page-panier.component.ts

--- src/app/component/panier/page-panier.component.ts
+++ src/app/component/panier/page-panier.component.ts
@@ -1,22 +1,44 @@
 import { Component } from '@angular/core';
 import { ManagerPanierService } from '../../service/manager-panier.service';
-
 import { environment } from '../../../environments/environment';
 import { Ligne } from '../../models/Ligne';
 
 @Component({
   selector: 'app-page-panier',
   templateUrl: './page-panier.component.html',
-  styleUrl: './page-panier.component.css'
+  styleUrls: ['./page-panier.component.css']
 })
 export class PagePanierComponent {
 
   panier: Array<Ligne>;
   pathImage = environment.apiGetImages;
+  total: number = 0;
 
   constructor(private managerPanierService: ManagerPanierService) {
     this.panier = managerPanierService.panier;
+    this.calculerTotal();
   }
 
+  ajouterQuantite(ligne: Ligne): void {
+    ligne.qte += 1;
+    this.managerPanierService.savePanier();
+  }
 
+  enleverQuantite(ligne: Ligne): void {
+    ligne.qte -= 1;
+    if (ligne.qte < 0) {
+      ligne.qte = 0; // Réinitialiser la quantité à zéro si elle devient négative
+    }
+    this.managerPanierService.savePanier(); // Appel de la fonction pour sauvegarder le panier
+  }
+  
+
+  supprimerLigne(ligne: Ligne): void {
+    this.managerPanierService.supprimerLigne(ligne);
+    this.panier = this.managerPanierService.panier; // Mettre à jour la variable panier
+  }
+
+  calculerTotal(): void {
+    this.total = this.panier.reduce((acc, ligne) => acc + ligne.qte * ligne.box.prix, 0);
+  }
 }
