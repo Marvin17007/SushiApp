99e52726239b67f721e77e139cdf26c28c737fa5
Marvin17007 <61842931+Marvin17007@users.noreply.github.com>
2024-04-23 22:59:02 +0200

    Update manager-panier.service.ts

--- src/app/service/manager-panier.service.ts
+++ src/app/service/manager-panier.service.ts
@@ -1,56 +1,64 @@
 import { Injectable } from '@angular/core';
-
 import { Box } from '../models/Box';
 import { Aliment } from '../models/Aliment';
 import { ApiSushiService } from './api-sushi.service';
 import { Ligne } from '../models/Ligne';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class ManagerPanierService {
 
-  panier:Array<Ligne>
+  panier: Array<Ligne>;
   CLE_PANIER: string = "panier";    // Nom de la clé pour localstorage.
-
+  managerPanierService: any;
+  
 
   constructor(private apiSushiService: ApiSushiService) {
-    this.panier=[]
-
-    if (localStorage.getItem(this.CLE_PANIER) != null) {
-      this.panier = JSON.parse(localStorage.getItem(this.CLE_PANIER) || "[]");
-    }
-
-    else {
-      this.panier = []
-    }
-  
+    this.panier = JSON.parse(localStorage.getItem(this.CLE_PANIER) || "[]");
   }
 
-
-
-
   addBox(addQte: number, newBox: Box): void {   
-    let ligne=new Ligne(addQte,newBox) 
-    let estPresent=false
+    let ligne = new Ligne(addQte, newBox); 
+    let estPresent = false;
   
     for (const uneLigne of this.panier) {
-      if(uneLigne.box.id==newBox.id){
-        estPresent=true
-        uneLigne.qte+=addQte
-      //TODO si qte =0 il faut supprimer la ligne du panier
+      if (uneLigne.box.id == newBox.id) {
+        estPresent = true;
+        uneLigne.qte += addQte;
+        // TODO si qte = 0 il faut supprimer la ligne du panier
       }
     }
 
-    if(estPresent==false){
-      this.panier.push(ligne)  
+    if (!estPresent) {
+      this.panier.push(ligne);
     }
   
-    //console.log(this.panier);
-    //console.log(JSON.stringify(this.panier));
-    //console.log(JSON.stringify(this.panier.articles.get(1)));
-    console.log(this.panier)
-     localStorage.setItem(this.CLE_PANIER, JSON.stringify(this.panier));  
+    this.savePanier();  
   }
+
+  ajouterQuantite(ligne: Ligne): void {
+    ligne.qte += 1;
+    this.savePanier();
+  }
+
+  enleverQuantite(ligne: Ligne): void {
+    ligne.qte -= 1;
+    // Si la quantité est inférieure à zéro, la réinitialiser à zéro
+    if (ligne.qte < 0) {
+      ligne.qte = 0;
+    }
+    this.savePanier();
+  }
+
+  supprimerLigne(ligne: Ligne): void {
+    this.panier = this.panier.filter(item => item !== ligne);
+    this.savePanier(); // Assurez-vous que savePanier() est correctement appelée ici
+  }
+
+  savePanier(): void {
+    localStorage.setItem(this.CLE_PANIER, JSON.stringify(this.panier));  
+  }
+
+
 }
